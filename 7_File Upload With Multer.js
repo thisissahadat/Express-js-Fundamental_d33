88f3f03c0cbf1b78b2ac7da9265b491c891bb7a1
thisissahadat <thisissahadat@gmail.com>
2023-03-12 23:37:51 +0600

    Commit By Shahadat Hossain 3/12/2023

---
+++ 7_File Upload With Multer.js
@@ -0,0 +1,34 @@
+var express=require('express');
+var multer=require('multer');
+var app=express();
+
+var storage=multer.diskStorage({
+    destination:function (req,file,callBack) {
+       callBack(null,'./uploads')
+    },
+    filename:function (req,file,callBack) {
+        callBack(null,file.originalname)
+    },
+})
+
+
+
+var upload=multer({storage:storage}).single('myfile')
+
+
+app.post('/',function (req, res) {
+    upload(req,res,function (error) {
+        if(error){
+            res.send("File upload Failed")
+        }
+        else {
+            res.send("File upload Successful")
+        }
+    })
+})
+
+
+
+app.listen(5000,function () {
+    console.log("server Running Successfully")
+})
